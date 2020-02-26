69034e44776b172295bafef65d45b0b1d632fefc
Ralph Schindler <ralph@ralphschindler.com>
2020-02-26 16:12:41 -0600

    Remove rouge console.log() from tool.js
    Remove rouge console.log() from tool.js

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -3,9 +3,7 @@ import ResourceTable from './components/ResourceTable'
 import ResourceTableRow from './components/ResourceTableRow'
 
 Nova.booting((Vue, router, store) => {
-
 	Vue.component('draggable', Draggable)
 	Vue.component('resource-table', ResourceTable)
 	Vue.component('resource-table-row', ResourceTableRow)
-console.log(Vue)
 })
